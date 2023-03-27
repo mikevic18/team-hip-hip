15f86ddf12963ce41be04c4e5aafb8715b1123a2
Rosie <rosie@greyhoundcs.com>
2023-03-27 14:59:51 +0100

    created skills model

--- backend/database/setup.sql
+++ backend/database/setup.sql
@@ -67,6 +67,8 @@ CREATE TABLE skill_share(
     title VARCHAR(255) NOT NULL,
     content VARCHAR(255) NOT NULL,
     video_url VARCHAR(255) NOT NULL,
+    votes INT DEFAULT 0,
+    creation_date timestamp,
     PRIMARY KEY(post_id),
     FOREIGN KEY (user_id) REFERENCES user_account("user_id")
 );
