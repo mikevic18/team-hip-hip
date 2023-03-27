f5f917f87f41fbf2819455de2f341e9714ef6b8a
Rosie <rosie@greyhoundcs.com>
2023-03-27 12:47:42 +0100

    created package.json

--- backend/database/setup.sql
+++ backend/database/setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS diary;
 DROP TABLE IF EXISTS user_account;
+
 CREATE TABLE user_account (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(30) UNIQUE NOT NULL,
