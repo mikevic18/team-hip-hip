d5df081eda6ec08ee77372d364a22db442f78948
britishbambi <josefjakubiak@gmail.com>
2023-03-29 11:35:08 +0100

    Update styling across site and sidebar functionality

---
+++ frontend/assets/js/navScript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("page-content").style.marginLeft = "150px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("page-content").style.marginLeft = "0";
+}
