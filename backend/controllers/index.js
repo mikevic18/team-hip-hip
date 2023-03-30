56f5c4232169f3121d47aa7e649b903532bf48d4
Michael T <56955591+mikevic18@users.noreply.github.com>
2023-03-30 09:24:08 +0100

    Rebasing branch

--- backend/controllers/index.js
+++
@@ -1,7 +0,0 @@
-exports.index = (req, res) => {
-    const data = {
-        title: "Back Bone of the Florin Council's Website! ",
-        description: "No peeking!",
-    };
-    res.status(200).json(data);
-};
